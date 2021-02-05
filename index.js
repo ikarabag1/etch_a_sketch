c4584d1779ae980b9ab11f12d13b3a181f5f7e3b
h64 <hhongnr@gmail.com>
2021-02-05 14:36:06 -0800

    solution code

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// The magic happens here!
+/* DOM References */
+const screen = document.querySelector('#screen')
+const clearBtn = document.querySelector('#clear-screen-button')
+
+/* Functions and Game Logic */
+const initialize = () => {
+    fill();
+}
+
+const fill = () => {
+    for(let i = 0; i < 9000; i++) {
+        let div = document.createElement('div');
+        div.classList.add('pixel')
+        div.addEventListener('mouseenter', shadePixel);
+        screen.appendChild(div);
+    }
+}
+
+const shadePixel = event => {
+    event.target.classList.add('colored-in')
+}
+
+const clear = event => {
+    for(pixel of screen.children) {
+        pixel.classList.remove('colored-in')
+    }
+}
+
+/* Event Listeners */
+document.addEventListener('DOMContentLoaded', initialize)
+clearBtn.addEventListener('click', clear)

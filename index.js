f8dbb9972854a52aa87911aae80ad4f5ee756807
TaylorDarneille <tmdarneille@gmail.com>
2022-02-03 08:05:58 -0800

    taylor's solution

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // The magic happens here!
+
+const numPixels = 5106
+
+const generatePixels = () => {
+    for(let i=0; i<numPixels; i++) {
+        const newPixel = document.createElement('div')
+        newPixel.classList.add('pixel')
+        makePixelLive(newPixel)
+        document.querySelector('#screen').appendChild(newPixel)
+    }
+}
+
+const makePixelLive = (pixel) => {
+    pixel.addEventListener('mouseenter', ()=>{
+        pixel.classList.add('colored-in')
+    })
+}
+
+const clearScreen = () => {
+    const pixels = document.querySelectorAll('.pixel')
+    for(let i=0; i<pixels.length; i++) {
+        pixels[i].classList.remove('colored-in') 
+    }
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    generatePixels()
+    document.querySelector('#clear-screen-button').addEventListener('click', clearScreen)
+})
